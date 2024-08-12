f482215b35fd632040e3df2bb5ad8d6f02138e18
Cinnamon Bun <nguyeney02@gmail.com>
2024-08-12 18:44:15 +0300

    WIP

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -71,7 +71,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start-prod',
+    command: 'npm run build && npm run start-prod',
     url: 'http://localhost:5001',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
