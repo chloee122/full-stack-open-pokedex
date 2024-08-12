3a0459c81fea280c570e16aa1002ae3a913841ea
Cinnamon Bun <nguyeney02@gmail.com>
2024-08-12 18:15:53 +0300

    WIP

--- playwright.config.js
+++ playwright.config.js
@@ -71,7 +71,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run start-prod',
     url: 'http://localhost:5001',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
